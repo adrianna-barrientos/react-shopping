e3822cddbffd875c77d7da198af0ee2bc84a136f
unknown <addie.barrientos110@gmail.com>
2024-10-30 10:10:56 -0500

    completed

---
+++ multi-counter/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MultiCounter from './components/MultiCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <MultiCounter />
+    </div>
+  );
+}
+
+export default App;
